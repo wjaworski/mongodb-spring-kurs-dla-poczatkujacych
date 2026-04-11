d3e2ef740ef6e24981f89541f85785685b07c61a
Wojciech Jaworski <jaworski.wojciech@gmail.com>
2026-04-11 13:57:31 +0200

    Fix integration tests configuration for SpringBoot 4

--- mongodb_basics/src/test/java/pl/jakubpradzynski/mongodb_basics/IntegrationTestsBase.java
+++ mongodb_basics/src/test/java/pl/jakubpradzynski/mongodb_basics/IntegrationTestsBase.java
@@ -2,12 +2,14 @@ package pl.jakubpradzynski.mongodb_basics;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class IntegrationTestsBase {
 
     @Container
